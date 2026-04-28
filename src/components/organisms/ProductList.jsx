04e18f96b63df25773e342013cbb18b25775b7b2
paucq <calderonpaula781@gmail.com>
2026-04-27 22:52:43 -0500

    feat: add product list organism

---
+++ src/components/organisms/ProductList.jsx
@@ -0,0 +1,11 @@
+import ProductCard from "../molecules/ProductCard.jsx";
+
+export default function ProductList({ products }) {
+  return (
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
